52863cb01aeda289deecd8da32cca8a067d9c6d1
Daniel Gann <gannd@fiu.edu>
2026-03-29 11:06:35 -0400

    envData_t1

---
+++ scripts/env_data_summary.R
@@ -0,0 +1,36 @@
+# File name: end_data_summary.R
+# Author: Dan Gann
+# email: gannd@fiu.edu
+
+# Required: Environmental data (xxxx.csv)
+#-------------------------------------------------------------------------------
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+#-------------------------------------------------------------------------------
+
+# DATA ISSUES ENCOUNTERED -- Inconsistency of habitat names; -999 for NA; non-random PSUs removed -- 513 and 220?
+
+#-------------------------------------------------------------------------------
+
+# load ENV data (Jay Sah raw data table converted to .csv modified column names)
+env <- read.csv('./data/processed/envData.csv')
+head(env)
+nrow(env)
+
+# generate PSU and PLOT_ID
+env <- separate(env, PLOT_ID, into = c('PSU','PLOT'),sep = c(4))
+
+# ------------------------------------------------------------------------------
+# plot ELE and save to jpeg
+psu.ELE.HAB <- ggplot(env, aes(x=PSU,y=GR_ELEV,fill=HABITAT)) +
+  geom_boxplot() +
+  # geom_text(aes(label=Freq),color="black",size=3) +
+  # scale_fill_gradient(low='white',high='slateblue') +
+  # scale_x_discrete(labels=c("Cycle 1","Cycle 2","Cycle 3")) +
+  labs(x='PSU', y='Ground Elevation (cm)',fill = "Habitat") +
+  theme_minimal()
+
+ggsave("./figures/psus_ELE_byHabitat.jpg", plot = psu.ELE.HAB, width = 10, height = 5, dpi = 400, device = "jpeg")
+#-------------------------------------------------------------------------------
+
